d528ad4ab9e44efa3dc8aa22422be2b39918f5fb
Leonelsciarratta <leonelsciarratta1@gmail.com>
2021-06-08 02:10:01 -0300

    vv
    hola

--- colegas.js
+++ colegas.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var colegas = [
     {nombre:"Adalid Alejandra <a href'https://alejandraadalid.githubv>",
 ]{
@@ -6,4 +7,14 @@ var colegas = [
     "Chinque Angelina": "https://angelinachinque9.github.io/curriculum/",
     "Sciarratta Leonel": "https://leonelsciarratta.github.io/Hotel/",
     "Louis Jeune, Esterline ": "https://esty-tech.github.io/Esty-cv/"
+=======
+var colegas = [
+    {nombre:"Adalid Alejandra <a href'https://alejandraadalid.githubv>",
+]{
+  bombre:  "Adalid Alejandra": "<a href='https://alejandraadalid.github.io/CV.Adalid/'> Adalid Alejandra </a>" ,
+    "Mayer Viegas Emiliano": "https://emimayerk.github.io/CV-Emiliano-Mayer-Viegas/",
+    "Chinque Angelina": "https://angelinachinque9.github.io/curriculum/",
+    "Sciarratta Leonel": "https://leonelsciarratta.github.io/Hotel/",
+    "Louis Jeune, Esterline ": "https://esty-tech.github.io/Esty-cv/"
+>>>>>>> Stashed changes
   }
--- dormitorios.js
+++ dormitorios.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var dormitorios = [{
     dormitorioID : 1,
     TipoDormitorio : "Simple",
@@ -13,4 +14,21 @@ var dormitorios = [{
         CategoryID : 1, 
     }
 }
+=======
+var dormitorios = [{
+    dormitorioID : 1,
+    TipoDormitorio : "Simple",
+    SupplierID : 1,
+    CategoryID : 1,
+    QuantityPerUnit : "10 boxes x 20 bags",
+    UnitPrice : 2000,
+    Ver : <a class="nav-link" href="simple.html">Simple</a>,
+    UnitsOnOrder : 0,
+    ReorderLevel : 10,
+    Discontinued : "Si",
+    Category : {
+        CategoryID : 1, 
+    }
+}
+>>>>>>> Stashed changes
 ]
