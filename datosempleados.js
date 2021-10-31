9b89e7619e00151b341c4b278e2593df4681cb61
Leonelsciarratta <69914692+Leonelsciarratta@users.noreply.github.com>
2021-10-31 10:54:06 -0300

    Update datosempleados.js

--- datosempleados.js
+++ datosempleados.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 
 var datosempleados = [{
   ProductID : 1,
@@ -32,39 +31,5 @@ var datosempleados = [{
       CategoryName : "Beverages",
       Description : "Soft drinks, coffees, teas, beers, and ales"
     }
-=======
 
-var datosempleados = [{
-  ProductID : 1,
-  ProductName : "Carlos Diaz",
-  SupplierID : 1,
-  CategoryID : 1,
-  QuantityPerUnit : "10 boxes x 20 bags",
-  UnitPrice : 28,
-  UnitsInStock : "2 anios",
-  UnitsOnOrder : 0,
-  ReorderLevel : 10,
-  Discontinued : "cocinero",
-  Category : {
-      CategoryID : 1,
-      CategoryName : "Beverages",
-      Description : "Soft drinks, coffees, teas, beers, and ales"
-  }
-}, {
-  ProductID : 2,
-  ProductName : "Beatriz Sosa",
-  SupplierID : 1,
-  CategoryID : 1,
-  QuantityPerUnit : "24 - 12 oz bottles",
-  UnitPrice : 49,
-  UnitsInStock : "12 anios",
-  UnitsOnOrder : 40,
-  ReorderLevel : 25,
-  Discontinued : "recepcionista",
-  Category : {
-      CategoryID : 1,
-      CategoryName : "Beverages",
-      Description : "Soft drinks, coffees, teas, beers, and ales"
-    }
->>>>>>> Stashed changes
 }];
