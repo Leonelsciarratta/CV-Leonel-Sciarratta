c5f9c1ab5d107a775167870acb60cc9f3f8057fa
Leonelsciarratta <69914692+Leonelsciarratta@users.noreply.github.com>
2021-10-31 10:57:11 -0300

    Update dormitorios.js

--- dormitorios.js
+++ dormitorios.js
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 var dormitorios = [{
     dormitorioID : 1,
     TipoDormitorio : "Simple",
@@ -14,21 +14,4 @@ var dormitorios = [{
         CategoryID : 1, 
     }
 }
-=======
-var dormitorios = [{
-    dormitorioID : 1,
-    TipoDormitorio : "Simple",
-    SupplierID : 1,
-    CategoryID : 1,
-    QuantityPerUnit : "10 boxes x 20 bags",
-    UnitPrice : 2000,
-    Ver : <a class="nav-link" href="simple.html">Simple</a>,
-    UnitsOnOrder : 0,
-    ReorderLevel : 10,
-    Discontinued : "Si",
-    Category : {
-        CategoryID : 1, 
-    }
-}
->>>>>>> Stashed changes
 ]
