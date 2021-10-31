93eb4e8c088c10e5b14be55efda4867a98dc5258
Leonelsciarratta <69914692+Leonelsciarratta@users.noreply.github.com>
2021-10-31 11:14:13 -0300

    Update trabajos.js

--- trabajos.js
+++ trabajos.js
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 var trabajos = [
     {    Anio : "2020",
       Empresa  : "Cilsa",
@@ -17,24 +17,4 @@ var trabajos = [
     Posicion : "Introduccion a la proramacion web"},
     {    Anio : "2016",
       Empresa  : "Escuela 404 Dr Lisandro De La Torre",
-=======
-var trabajos = [
-    {    Anio : "2020",
-      Empresa  : "Cilsa",
-    Posicion : "Desarrollo Front End"},
-    {    Anio : "2020",
-    Empresa  : "Fundacion Telefonica",
-  Posicion : "Programacion con Java Standard"},
-    {    Anio : "2020",
-      Empresa  : "Cilsa",
-    Posicion : "Marketing Digital y wordpress"},
-    {    Anio : "2019",
-      Empresa  : "Terciario Urquiza",
-    Posicion : "Tecnico Superior en Analisis Funcional de Sistemas Informaticos"},
-    {    Anio : "2019",
-      Empresa  : "Comunidad IT",
-    Posicion : "Introduccion a la programacion web"},
-    {    Anio : "2016",
-      Empresa  : "Escuela 404 Dr Lisandro De La Torre",
->>>>>>> Stashed changes
-    Posicion : "Secundario con orientación Informatica"}];
+}];
